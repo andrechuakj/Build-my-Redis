8b7fccbd76401507d00ea038346aa36e2b76cb24
Andre Chua <110965035+andrechuakj@users.noreply.github.com>
2024-02-21 17:28:07 +0800

    Add ping function

--- app/main.js
+++ app/main.js
@@ -4,8 +4,25 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
-const server = net.createServer((connection) => {
-  // Handle connection
+const server = net.createServer((socket) => {
+  console.log(
+    "Connection from",
+    socket.remoteAddress,
+    "port",
+    socket.remotePort
+  );
+
+  socket.on("data", (buffer) => {
+    console.log(
+      "Request from",
+      socket.remoteAddress,
+      "port",
+      socket.remotePort
+    );
+    if (buffer.toString("utf-8") === "*1\r\n$4\r\nping\r\n") {
+      socket.write("+PONG\r\n");
+    }
+  });
 });
 
 server.listen(6379, "127.0.0.1");
