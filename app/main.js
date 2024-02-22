b99b1ed25852147fc89394a3761666de3de55932
root <root@lenovoAC.localdomain>
2024-02-22 21:07:11 +0800

    CLI tests (cli-test-1708607231697)

--- app/main.js
+++ app/main.js
@@ -15,11 +15,36 @@ const server = net.createServer((socket) => {
       "port",
       socket.remotePort
     );
-    if (buffer.toString("utf-8") === "*1\r\n$4\r\nping\r\n") {
-      socket.write("+PONG\r\n");
+    command, parsedData = parse(buffer);
+    switch (command) {
+      case "ping":
+        socket.write("+PONG\r\n");
+        break;
+      case "echo":
+        socket.write(parse(parsedData[0]));
+        break;
+      default:
+        socket.write("-ERR unknown command\r\n");
     }
   });
 });
 
 server.listen(6379, "127.0.0.1");
 
+function parse(buffer) {
+  const data = buffer.toString("utf-8").split("\r\n");
+  data.pop();
+  const command = data.shift().toLowerCase();
+  parsedData = [];
+  for (let i = 1; i < data.length; i += 2) {
+    parsedData.push(data[i]);
+  }
+  return { command, parsedData };
+}
+
+function unparse(data) {
+  for (let i = 0; i < data.length; i++) {
+    data[i] = `$${data[i].length}\r\n${data[i]}`;
+  }
+  return `*${data.length}\r\n${data.join("\r\n")}\r\n`;
+}
